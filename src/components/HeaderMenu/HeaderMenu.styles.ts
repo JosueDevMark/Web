6d33c99bfb0bb28ba2dc811762215197f23e51e2
Josué Us <ujosuegt@outlook.com>
2018-01-13 17:55:06 -0600

    Implements glamor and set basic nav bar

---
+++ src/components/HeaderMenu/HeaderMenu.styles.ts
@@ -0,0 +1,5 @@
+import { css } from 'glamor';
+
+export const header = css({
+    backgroundColor: "#212121"
+});
--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -2,31 +2,51 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { toggleSidebar } from "../../store";
-import { Container, Label, Menu, Icon } from "semantic-ui-react";
+import { Container, Label, Menu, Image } from "semantic-ui-react";
 import { MenuProps } from "../Menu";
+import { css } from 'glamor';
+import * as logo from "../../images/logo-transparent.svg";
+import * as Styles from "./HeaderMenu.styles";
 
 interface HeaderMenuProps extends MenuProps {
   dispatch: Dispatch<any>;
   inverted?: boolean;
 }
 
-export const HeaderMenu = ({ items, pathname, Link, inverted, dispatch }: HeaderMenuProps) =>
-  <Container>
-    <Menu size="large" pointing secondary inverted={inverted}>
-      <Menu.Item as="a" className="mobile only" icon="sidebar" onClick={() => dispatch(toggleSidebar())} />
-      <Menu.Item className="mobile hidden"><Icon name="spy" size="big" /></Menu.Item>
-      {items.map((item) => {
-        const active = (item.exact) ? pathname === item.path : pathname.startsWith(item.path);
-        return <Menu.Item
-          as={Link}
-          className="mobile hidden"
-          name={item.name}
-          to={item.path}
-          key={item.path}
-          active={active}
-        />;
-      })}
-    </Menu>
-  </Container>;
+export class HeaderMenu extends React.Component<HeaderMenuProps> {
+
+  handleMenu = () => this.props.dispatch(toggleSidebar());
+
+  render() {
+    const { items, pathname, Link, inverted, dispatch } = this.props;
+
+    return (
+      <Container>
+        <Menu
+          size="large"
+          pointing
+          secondary
+          inverted={inverted}
+          className={`${Styles.header}`}
+          borderless={true}
+        >
+          <Menu.Item as="a" className="mobile only" id="about" icon="sidebar" onClick={this.handleMenu} />
+          {items.map(item => {
+            const active = item.exact ? pathname === item.path : pathname.startsWith(item.path);
+            return <Menu.Item
+              as={Link}
+              className="mobile hidden"
+              name={item.name}
+              to={item.path}
+              key={item.path}
+              active={active}
+            />;
+          })}
+          <Image src={logo} />
+        </Menu>
+      </Container>
+    );
+  }
+};
 
 export default connect()(HeaderMenu);
